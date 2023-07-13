46a995106b2f1d8edbb5fa27b1e22e60c6660ad8
Seonghun <tjdwjs23@abc.ne.kr>
2023-07-13 16:22:26 +0900

    .idea delete

--- build/tmp/kapt3/stubs/main/test/example/tokenapi/token/entity/TemplateRepository.java
+++
@@ -1,8 +0,0 @@
-package test.example.tokenapi.token.entity;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\t\n\u0000\bg\u0018\u00002\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u0001\u00a8\u0006\u0004"}, d2 = {"Ltest/example/tokenapi/token/entity/TemplateRepository;", "Lorg/springframework/data/jpa/repository/JpaRepository;", "Ltest/example/tokenapi/token/entity/Template;", "", "kotlinTokenApi"})
-@org.springframework.stereotype.Repository()
-public abstract interface TemplateRepository extends org.springframework.data.jpa.repository.JpaRepository<test.example.tokenapi.token.entity.Template, java.lang.Long> {
-}
